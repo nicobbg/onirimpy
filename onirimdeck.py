51ff1584fba7f818098f78477927899a36952db8
nicobbg <nicobbg@gmail.com>
2014-06-04 15:16:06 +0200

    modify the init method for a deck to add 10 nightmare cards

--- onirimdeck.py
+++ onirimdeck.py
@@ -1,15 +1,24 @@
+from onirimcard import Card
+
 class Deck:
     """
     This is a Deck.
-    A Deck is composed of xx cards : nn Labyrinth cards, 8 Door cards and mm Nightmare cards.
+    A Deck is a list of 76 cards : 58 Labyrinth cards, 8 Door cards and 10 Nightmare cards.
     """
+    deck = []
 
     def __init__(self):
-        deck = []
-        colour = ['blue', 'red', 'white', 'green']
-        for count in range(0, 1): 
-            for i in colour:
-                a_door = Door(i)
-                deck.append(a_door)
+        for i in range(0, 9):
+            self.deck.append(Card('nightmare'))
 
+    def get_deck(self):
+    """
+    This method print the content of the deck card by card
+    """
+        print self.deck
+
+    def shuffle_deck(self):
+    """
+    This method shuffle the whole deck
+    """
 
